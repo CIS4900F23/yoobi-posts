4f16a32faf8ad73230f4e00d4ede1ca7d3dc0ef6
loganhagen <loganhagen@gmail.com>
2023-10-30 09:56:27 -0400

    first commit

---
+++ src/api/__init__.py
@@ -0,0 +1,18 @@
+"""Initializes our API blueprint for our Flask app"""
+import os
+from flask import Blueprint
+from flask_restx import Api
+from src.api.health import api as health_ns
+from src.api.posts import api as posts_ns
+from src.api.plot import api as plot_ns
+
+# Create an API blueprint
+api_bp = Blueprint("api", __name__)
+
+# Create an API object with a title and description
+api = Api(api_bp, title="Flask REST API", description="A RESTapi built with Flask")
+
+# Add api namespaces to the api object that was created
+api.add_namespace(health_ns)
+api.add_namespace(posts_ns)
+api.add_namespace(plot_ns)
