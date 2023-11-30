bee4bc28d4e6d0fead941d1315019a548ce1dd44
loganhagen <loganhagen@gmail.com>
2023-11-29 20:45:17 -0500

    do not populate db

--- populateDB.py
+++
@@ -1,7 +0,0 @@
-from src.db.connect import DBConnect
-
-dbc = DBConnect()
-#dbc.dropCollection("UBI", "reddit_posts")
-#print(dbc.getCollectionNames("UBI"))
-#dbc.writeFileToCollection("UBI", "tweets", "tweets.json")
-dbc.writeFileToCollection("UBI", "reddit_posts", "reddit_posts.json")
